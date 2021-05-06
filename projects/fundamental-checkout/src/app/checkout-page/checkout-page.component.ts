60d416f20082b9f45e1a9c516bd8ef88780408fe
Kevin Okamoto <kevin.okamoto@sap.com>
2021-05-06 12:12:50 -0700

    Adding datasource for addresses

--- projects/fundamental-checkout/src/app/checkout-page/checkout-page.component.ts
+++ projects/fundamental-checkout/src/app/checkout-page/checkout-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { CheckoutService } from '../checkout.service';
-import { Requisition } from '../models';
+import { Address, Requisition } from '../models';
 
 @Component({
   selector: 'app-checkout-page',
@@ -12,14 +12,20 @@ import { Requisition } from '../models';
 export class CheckoutPageComponent implements OnInit {
 
   formGroup: FormGroup;
+  formGroup2: FormGroup;
   requisition$: Observable<Requisition>;
+  addresses$: Observable<Address[]>;
 
   constructor(private service: CheckoutService) {
   }
 
   ngOnInit(): void {
     this.formGroup = new FormGroup({});
+    this.formGroup2 = new FormGroup({});
+
     this.requisition$ = this.service.getRequisition();
+    this.addresses$ = this.service.getAddresses();
+
   }
 
 }
